3e4515a395cd8f54c8ef94343e6cd65e6f08a591
MFC-MiguelFerreira <mfc.miguelferreira@gmail.com>
2025-08-02 15:09:13 -0300

    🎉  begin atividade2 using terraform as IaC tool to create the S3 buckets

---
+++ atividade2/terraform/variable.tf
@@ -0,0 +1,11 @@
+variable "aws_profile_name" {
+  type        = string
+  description = "AWS profile locally configured with aws configure or aws configure sso command."
+  nullable    = false
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region to deploy the infrastructure."
+  nullable    = false
+}
