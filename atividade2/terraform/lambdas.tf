78d7b53f4f3285b9205515199e7fc21f001a0ac6
MFC-MiguelFerreira <mfc.miguelferreira@gmail.com>
2025-08-05 22:21:11 -0300

    ✨ Add Lambda function to insert data into PostgreSQL: implement S3 integration and database connection

--- atividade2/terraform/lambdas.tf
+++ atividade2/terraform/lambdas.tf
@@ -123,3 +123,46 @@ module "lambda_function_result" {
   create_role = false
   lambda_role = "arn:aws:iam::${local.account_id}:role/LabRole"
 }
+
+module "lambda_function_to_db" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  environment_variables = {
+    delivery_bucket_name = aws_s3_bucket.delivery_datalake_bucket.bucket,
+  }
+
+  function_name = "to_db"
+  handler       = "to_db.lambda_handler"
+  runtime       = "python3.12"
+  memory_size   = 1024
+  ephemeral_storage_size = 512
+  timeout       = 300
+  publish       = true
+
+  source_path = ["../lambdas/scripts/to_db/to_db.py"]
+
+  store_on_s3 = true
+  s3_bucket   = aws_s3_bucket.lambda_bucket.bucket
+
+  create_layer = false
+  layers       = [module.lambda_layer_s3_to_db.lambda_layer_arn]
+
+  ######################
+  # Additional policies
+  ######################
+  create_role = false
+  lambda_role = "arn:aws:iam::${local.account_id}:role/LabRole"
+
+  # ✅ VPC Configuration to access Aurora
+  vpc_subnet_ids         = [
+    "subnet-03cacd8897dbb5e9f",
+    "subnet-07bd42120c3d1d120",
+    "subnet-0b38f6e311d149cbf",
+    "subnet-0173a2cec145c7e6d",
+    "subnet-0844c11dda16c40ee",
+    "subnet-0421061737a616939",
+  ]     # Replace with your actual subnet IDs
+  vpc_security_group_ids = [
+    "sg-0135aebcef6d4d241"
+  ]  # Create one below or use existing
+}
